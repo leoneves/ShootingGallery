9025f679905a2b3fc32de1b5d1ec1daf65e238f5
leoneves <eleonardoneves@gmail.com>
2019-02-25 23:42:01 -0300

    msbuild

--- build.cmd
+++ build.cmd
@@ -1,8 +1,6 @@
 @echo off
 
-nuget "install" "xunit.runner.console" "-OutputDirectory" "tools" "-ExcludeVersion"
-nuget "install" "FAKE.Core" "-OutputDirectory" "tools" "-ExcludeVersion"
 nuget restore
 cd ShootingGallery
 
-:Build
+msbuild
